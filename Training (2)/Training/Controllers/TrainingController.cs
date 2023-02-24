78fbd764df7b35481f6de35a223d55054f89b28b
SofiaHjerpe <hjerpesofia@gmail.com>
2023-02-24 17:04:13 +0100

    COMMIT

---
+++ Training (2)/Training/Controllers/TrainingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Training.Models;
+
+namespace Training.Controllers
+{
+    public class TrainingController : Controller
+    {
+       
+            public async Task<IActionResult> Index()
+            {
+                Database db = new Database();
+                var mytraining = await db.GetTrainingsAsync();
+                return View(mytraining);
+            }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MyTrainings training)
+        {
+            Database db = new Database();
+            await db.SaveTraining(training);
+            return Redirect("/Training");
+        }
+
+
+    }
+}
